b148e3a5b59c47f555b5ad41b7477f8584e68839
remi128 <mremme@braintags.de>
2015-06-26 15:02:30 +0200

    initphase

---
+++ vertx-pojo-mapper-common/src/main/java/io/vertx/ext/pojomapper/IDataStore.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2014 Red Hat, Inc.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * 
+ * The Eclipse Public License is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * The Apache License v2.0 is available at
+ * http://www.opensource.org/licenses/apache2.0.php
+ * 
+ * You may elect to redistribute this code under either of these licenses.
+ */
+
+package io.vertx.ext.pojomapper;
+
+/**
+ * IDataStore contains information about the destination datastore and creates the handler objects
+ * 
+ * @author Michael Remme
+ * 
+ */
+
+public interface IDataStore {
+
+  /**
+   * Returns a new {@link IQuery} bound to the given mapper
+   * 
+   * @param mapper
+   *          the mapper class, where the new instance shall be bound to
+   * @return a new instance of {@link IWrite}
+   */
+  <T> IQuery<T> createQuery(Class<T> mapper);
+
+  /**
+   * Create a new {@link IWrite} bound to the given mapper
+   * 
+   * @param mapper
+   *          the mapper class, where the new instance shall be bound to
+   * @return a new instance of {@link IWrite}
+   */
+  <T> IWrite<T> createWrite(Class<T> mapper);
+
+  /**
+   * Create a new {@link IDelete} bound to the given mapper
+   * 
+   * @param mapper
+   *          the mapper class, where the new instance shall be bound to
+   * @return a new instance of {@link IWrite}
+   */
+  <T> IDelete<T> createDelete(Class<T> mapper);
+
+  /**
+   * Get or create the {@link IMapperFactory} used by this implementation
+   * 
+   * @return
+   */
+  IMapperFactory getMapperFactory();
+
+}
---
+++ vertx-pojo-mapper-common/src/main/java/io/vertx/ext/pojomapper/IWrite.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2014 Red Hat, Inc.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * 
+ * The Eclipse Public License is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * The Apache License v2.0 is available at
+ * http://www.opensource.org/licenses/apache2.0.php
+ * 
+ * You may elect to redistribute this code under either of these licenses.
+ */
+
+package io.vertx.ext.pojomapper;
+
+/**
+ * IWrite is responsible for all write actions into the connected datasource. It performs inserts and updates.
+ * 
+ * @author Michael Remme
+ * 
+ */
+
+public interface IWrite<T> extends IDataAccessObject<T> {
+
+}
