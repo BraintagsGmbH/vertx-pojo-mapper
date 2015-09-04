e8cb261e3315334ec3799ff80c272b47049943b3
remi128 <mremme@braintags.de>
2015-09-04 14:57:28 +0200

    docs

--- vertx-pojongo/src/main/java/de/braintags/io/vertx/pojomapper/mongo/package-info.java
+++ vertx-pojongo/src/main/java/de/braintags/io/vertx/pojomapper/mongo/package-info.java
@@ -17,8 +17,7 @@
  * We provide an implementation of {@link de.braintags.io.vertx.pojomapper.IDataStore} which uses the Vert.x {@link io.vertx.ext.mongo.MongoClient}
  * to perform mapping of POJOs into the datastore
  *
- * To create an instance you first need an instance of {@link io.vertx.ext.mongo.MongoClient}. 
- * To learn how to create one, please consult the documentation for the MongoClient.
+ * To create an instance you first need an instance of {@link io.vertx.ext.mongo.MongoClient}. To learn how to create one, please consult the documentation for the MongoClient.
  *
  * Once you've got one, you can create an instance of {@link de.braintags.io.vertx.pojomapper.IDataStore}, which deals with MongoDB,
  * as follows:
@@ -30,6 +29,8 @@
  * 
  * The rest of the usage is the same than described in the documentation of 
  * https://github.com/BraintagsGmbH/vertx-pojo-mapper/tree/master/vertx-pojo-mapper-common 
+ * 
+ * https://github.com/BraintagsGmbH/vertx-pojo-mapper/blob/master/vertx-pojo-mapper-common/src/main/asciidoc/java/index.adoc[vertx-pojo-mapper-common]
  * 
  * @author Michael Remme
  * 
