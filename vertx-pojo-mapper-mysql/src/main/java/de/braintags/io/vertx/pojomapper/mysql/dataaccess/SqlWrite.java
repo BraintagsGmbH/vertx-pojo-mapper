1d616a155b31995426bdeb5491cd396e00d6fbfe
remi128 <mremme@braintags.de>
2015-09-25 16:29:44 +0200

    LAST_INSERT_ID

--- vertx-pojo-mapper-mysql/src/main/java/de/braintags/io/vertx/pojomapper/mysql/dataaccess/SqlWrite.java
+++ vertx-pojo-mapper-mysql/src/main/java/de/braintags/io/vertx/pojomapper/mysql/dataaccess/SqlWrite.java
@@ -123,6 +123,16 @@ public class SqlWrite<T> extends AbstractWrite<T> {
     Object currentId = storeObject.get(mapper.getIdField());
     LOGGER.info("now saving");
 
+    /*
+     * LAST_INSERT_ID The ID that was generated is maintained in the server on a per-connection basis. This means that
+     * the value returned by the function to a given client is the first AUTO_INCREMENT value generated for most recent
+     * statement affecting an AUTO_INCREMENT column by that client. This value cannot be affected by other clients, even
+     * if they generate AUTO_INCREMENT values of their own. This behavior ensures that each client can retrieve its own
+     * ID without concern for the activity of other clients, and without the need for locks or transactions.
+     * 
+     * 
+     */
+
     if (currentId == null) {
       // INSERT INTO tbl_name VALUES (1, "row 1"), (2, "row 2");
 
