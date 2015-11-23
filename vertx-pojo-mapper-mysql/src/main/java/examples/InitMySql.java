b121239eff6155c5f534d99f7920ce4df42c0960
remi128 <mremme@braintags.de>
2015-11-23 16:52:34 +0100

    documentation

---
+++ vertx-pojo-mapper-mysql/src/main/java/examples/InitMySql.java
@@ -0,0 +1,43 @@
+/*
+ * #%L
+ * vertx-pojongo
+ * %%
+ * Copyright (C) 2015 Braintags GmbH
+ * %%
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * #L%
+ */
+package examples;
+
+import java.util.Objects;
+
+import de.braintags.io.vertx.pojomapper.mapping.IKeyGenerator;
+import de.braintags.io.vertx.pojomapper.mapping.impl.keygen.FileKeyGenerator;
+import de.braintags.io.vertx.pojomapper.mysql.MySqlDataStore;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.docgen.Source;
+import io.vertx.ext.asyncsql.AsyncSQLClient;
+import io.vertx.ext.asyncsql.MySQLClient;
+
+@Source(translate = false)
+public class InitMySql {
+  private MySqlDataStore datastore;
+  private AsyncSQLClient mySQLClient;
+
+  public void initMySqlClient(Vertx vertx, String username, String password, String database) {
+    Objects.requireNonNull(username, "Username is required");
+    Objects.requireNonNull(password, "Password is required");
+    Objects.requireNonNull(database, "database is required");
+
+    JsonObject mySQLClientConfig = new JsonObject().put("host", "localhost").put("username", username)
+        .put("password", password).put("database", database).put("port", 3306)
+        .put(IKeyGenerator.DEFAULT_KEY_GENERATOR, FileKeyGenerator.NAME);
+
+    mySQLClient = MySQLClient.createShared(vertx, mySQLClientConfig);
+    datastore = new MySqlDataStore(vertx, mySQLClient, mySQLClientConfig);
+  }
+}
---
+++ vertx-pojongo/src/main/java/examples/InitMongoDatastore.java
@@ -0,0 +1,34 @@
+/*
+ * #%L
+ * vertx-pojongo
+ * %%
+ * Copyright (C) 2015 Braintags GmbH
+ * %%
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * #L%
+ */
+package examples;
+
+import java.util.Objects;
+
+import de.braintags.io.vertx.pojomapper.mongo.MongoDataStore;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.docgen.Source;
+import io.vertx.ext.mongo.MongoClient;
+
+@Source(translate = false)
+public class InitMongoDatastore {
+
+  public void initMongoDatastore(Vertx vertx, String database) {
+    Objects.requireNonNull(database, "database is required");
+    JsonObject config = new JsonObject();
+    config.put("connection_string", "mongodb://localhost:27017");
+    config.put("db_name", database);
+    MongoClient mongoClient = MongoClient.createNonShared(vertx, config);
+    MongoDataStore mongoDataStore = new MongoDataStore(vertx, mongoClient, config);
+  }
+}
