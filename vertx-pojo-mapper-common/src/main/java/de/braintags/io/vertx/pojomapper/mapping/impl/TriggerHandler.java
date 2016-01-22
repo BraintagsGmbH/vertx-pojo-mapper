68f8310f7bb6abbfe2993018ec27e0224a278fdd
remi128 <mremme@braintags.de>
2016-01-22 12:42:38 +0100

    adding parameters for triggers

--- vertx-pojo-mapper-common/src/main/java/de/braintags/io/vertx/pojomapper/mapping/impl/TriggerHandler.java
+++ vertx-pojo-mapper-common/src/main/java/de/braintags/io/vertx/pojomapper/mapping/impl/TriggerHandler.java
@@ -12,33 +12,40 @@
  */
 package de.braintags.io.vertx.pojomapper.mapping.impl;
 
+import de.braintags.io.vertx.pojomapper.annotation.lifecycle.AfterSave;
+import de.braintags.io.vertx.pojomapper.annotation.lifecycle.BeforeSave;
 import de.braintags.io.vertx.pojomapper.mapping.IMapper;
+import de.braintags.io.vertx.pojomapper.mapping.ITriggerHandler;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 
 /**
- * 
+ * A TriggerHandler can be used as argument for mapper methods, which are annotated by one of the annotations like
+ * {@link BeforeSave}, {@link AfterSave} etc.
  * 
  * @author Michael Remme
  * 
  */
-public class TriggerHandler implements Handler<AsyncResult<Void>> {
+public class TriggerHandler implements ITriggerHandler {
   private IMapper mapper;
+  private Handler<AsyncResult<Void>> handler;
 
   /**
    * 
    */
-  public TriggerHandler(IMapper mapper) {
+  public TriggerHandler(IMapper mapper, Handler<AsyncResult<Void>> handler) {
     this.mapper = mapper;
+    this.handler = handler;
   }
 
-  /*
-   * (non-Javadoc)
+  /**
+   * Get the instance of IMapper, which is underlaying the current request
    * 
-   * @see io.vertx.core.Handler#handle(java.lang.Object)
+   * @return the mapper
    */
   @Override
-  public void handle(AsyncResult<Void> event) {
+  public final IMapper getMapper() {
+    return mapper;
   }
 
 }
