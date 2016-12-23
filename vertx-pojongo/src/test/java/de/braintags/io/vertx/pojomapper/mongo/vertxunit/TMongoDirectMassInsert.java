ab35470ffd2c5df56bda696be79d52f9b8f15059
Michael Remme <mremme@braintags.de>
2016-12-23 11:59:54 +0100

    extracted mongo client

--- vertx-pojongo/src/test/java/de/braintags/io/vertx/pojomapper/mongo/vertxunit/TMongoDirectMassInsert.java
+++ vertx-pojongo/src/test/java/de/braintags/io/vertx/pojomapper/mongo/vertxunit/TMongoDirectMassInsert.java
@@ -12,28 +12,37 @@
  */
 package de.braintags.io.vertx.pojomapper.mongo.vertxunit;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import de.braintags.io.vertx.pojomapper.mongo.MongoDataStore;
-import de.braintags.io.vertx.pojomapper.testdatastore.DatastoreBaseTest;
-import de.braintags.io.vertx.pojomapper.testdatastore.TestHelper;
 import de.braintags.io.vertx.util.CounterObject;
 import de.braintags.io.vertx.util.ErrorObject;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.IMongodConfig;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
 import io.zatarox.vertx.async.AsyncFactorySingleton;
 import io.zatarox.vertx.async.utils.DefaultAsyncResult;
 
@@ -44,62 +53,58 @@ import io.zatarox.vertx.async.utils.DefaultAsyncResult;
  * 
  */
 
-public class TMongoDirectMassInsert extends DatastoreBaseTest {
+@RunWith(VertxUnitRunner.class)
+public class TMongoDirectMassInsert {
   private static Logger LOGGER = LoggerFactory.getLogger(TMongoDirectMassInsert.class);
   private static final int LOOP = 500;
   private static final List<Integer> LOOPLIST = IntStream.iterate(0, i -> i + 1).limit(LOOP).boxed()
       .collect(Collectors.toList());
 
   private static final String TABLENAME = "massInsert";
+  private static final boolean startMongoLocal = true;
   private static List<String> results = new ArrayList<>();
+  private static MongoClient client;
+  private static Vertx vertx;
+  public static final String CONNECTION_STRING_PROPERTY = "connection_string";
+  public static final String DEFAULT_CONNECTION = "mongodb://localhost:27017";
+  public static final String DBNAME_PROP = "db_name";
+  private static MongodExecutable exe;
+  private static int localPort = 27017;
 
   @Test
   public void massInsertWithCounter(TestContext context) {
-    ErrorObject err = new ErrorObject<>(null);
     Async async = context.async();
     dropTable(context, TABLENAME);
-    MongoDataStore ds = (MongoDataStore) getDataStore(context);
-    MongoClient client = (MongoClient) ds.getClient();
 
     final long startTime = System.currentTimeMillis();
-    CounterObject<Void> co = new CounterObject<>(LOOP, result -> {
-      if (result.failed()) {
-        LOGGER.error("", result.cause());
-        err.setThrowable(result.cause());
-        async.complete();
-      } else {
-        LOGGER.info("finished");
-        client.count(TABLENAME, new JsonObject(), cr -> {
-          if (cr.failed()) {
-            LOGGER.error("", cr.cause());
-            err.setThrowable(cr.cause());
-            async.complete();
-          } else {
-            try {
-              context.assertEquals(LOOP, cr.result().intValue(), "result not correct");
-              async.complete();
-            } catch (Exception e) {
-              LOGGER.error("", e);
-              err.setThrowable(e);
-              async.complete();
-            }
-          }
-        });
-      }
-    });
+    CounterObject<Void> co = new CounterObject<>(LOOP, null);
     for (int i = 0; i < LOOP; i++) {
       JsonObject jsonCommand = new JsonObject().put("name", "testName " + i);
       client.insert(TABLENAME, jsonCommand, result -> {
         if (result.failed()) {
-          co.setThrowable(result.cause());
-          err.setThrowable(result.cause());
           LOGGER.error("", result.cause());
+          co.setThrowable(result.cause());
           async.complete();
         } else {
           LOGGER.info("executed: " + result.result());
           if (co.reduce()) {
-            co.setResult(null);
-            async.complete();
+            LOGGER.info("finished");
+            client.count(TABLENAME, new JsonObject(), cr -> {
+              if (cr.failed()) {
+                LOGGER.error("", cr.cause());
+                co.setThrowable(cr.cause());
+                async.complete();
+              } else {
+                try {
+                  context.assertEquals(LOOP, cr.result().intValue(), "result not correct");
+                  async.complete();
+                } catch (Exception e) {
+                  LOGGER.error("", e);
+                  co.setThrowable(e);
+                  async.complete();
+                }
+              }
+            });
           }
         }
       });
@@ -113,7 +118,7 @@ public class TMongoDirectMassInsert extends DatastoreBaseTest {
       context.fail(co.getThrowable());
     }
     long execution = System.currentTimeMillis() - startTime;
-    results.add("massInsertWithCounter" + execution + " | ");
+    results.add("massInsertWithCounter: " + execution + " | ");
     LOGGER.info(results.toString());
   }
 
@@ -122,11 +127,8 @@ public class TMongoDirectMassInsert extends DatastoreBaseTest {
     ErrorObject err = new ErrorObject<>(null);
     Async async = context.async();
     dropTable(context, TABLENAME);
-    MongoDataStore ds = (MongoDataStore) getDataStore(context);
-    MongoClient client = (MongoClient) ds.getClient();
-
     final long startTime = System.currentTimeMillis();
-    Context c = TestHelper.vertx.getOrCreateContext();
+    Context c = vertx.getOrCreateContext();
     if (c == null) {
       throw new NullPointerException();
     }
@@ -171,8 +173,57 @@ public class TMongoDirectMassInsert extends DatastoreBaseTest {
     }
 
     long execution = System.currentTimeMillis() - startTime;
-    results.add("massInsertWithAsync" + execution + " | ");
+    results.add("massInsertWithAsync: " + execution + " | ");
     LOGGER.info(results.toString());
+  }
+
+  @BeforeClass
+  public static void beforeTest() {
+    vertx = Vertx.vertx();
+    startMongoExe();
+    client = MongoClient.createShared(vertx, createConfig());
+  }
+
+  private static JsonObject createConfig() {
+    JsonObject config = new JsonObject();
+    config.put("connection_string", System.getProperty(CONNECTION_STRING_PROPERTY, DEFAULT_CONNECTION));
+    config.put(DBNAME_PROP, TMongoDirectMassInsert.class.getSimpleName());
+    return config;
+  }
+
+  public static void dropTable(TestContext context, String collection) {
+    Async async = context.async();
+    ErrorObject<Void> err = new ErrorObject<>(null);
+    client.dropCollection(collection, dropResult -> {
+      if (dropResult.failed()) {
+        err.setThrowable(dropResult.cause());
+        async.complete();
+      } else {
+        async.complete();
+      }
+    });
+  }
+
+  /**
+   * Starts an instance of a local mongo, if startMongoLocal is true
+   * 
+   * @param startMongoLocal
+   *          defines wether to start a local instance
+   * @param localPort
+   *          the port where to start the instance
+   */
+  private static void startMongoExe() {
+    if (exe == null && startMongoLocal) {
+      LOGGER.info("STARTING LOCAL MONGO");
+      try {
+        IMongodConfig config = new MongodConfigBuilder().version(Version.Main.PRODUCTION)
+            .net(new Net(localPort, Network.localhostIsIPv6())).build();
+        exe = MongodStarter.getDefaultInstance().prepare(config);
+        exe.start();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
   }
 
 }
